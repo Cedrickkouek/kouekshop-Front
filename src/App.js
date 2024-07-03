a32e928f5503eecaf2bd6176cb47d4784fe66983
unknown <cedrickkouek@gmail.com>
2024-07-03 17:45:11 +0200

    Done with the static nav bar

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+import Nav from './customer/components/navigation/nav';
+
+
+function App() {
+  return (
+    <div className="">
+      <Nav />
+    </div>
+  );
+}
+
+export default App;
